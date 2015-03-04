59b7e06c402e0b8fde63f82b7f1f250ec352e98b
Tatiyana Domanova <tdomanova@exadel.com>
2015-03-04 20:03:01 +0300

    Divided subgraphs by layers

--- HasseDiagram/src/core/Visualizator.java
+++ HasseDiagram/src/core/Visualizator.java
@@ -4,7 +4,10 @@ import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Image;
 import java.awt.image.BufferedImage;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.swing.ImageIcon;
 
@@ -38,20 +41,42 @@ public class Visualizator {
 				g.drawString("Third condition: E != V - 1", width / 2 - 50,
 						height / 2);
 			} else {
-				printSSubGraphs(graph, g);
+				printSgraphsMap(graph, g);
 			}
 		}
 
 		return new ImageIcon(img);
 	}
+	
+	private static void printSgraphsMap(HyperGraph h, Graphics g) {
+		Map<Integer, List<SubGraph>> map = getLayers(h);
+		int i = 0;
+		for (List<SubGraph> list: map.values()) {
+			g.drawString(list.toString(), 10, i++ * 20 + 10);
+		}
+	}
+
+	private static Map<Integer, List<SubGraph>> getLayers(HyperGraph h) {
+		List<SubGraph> list = h.getSSubGraphs();
+		Map<Integer, List<SubGraph>> map = new HashMap<>();
+		
+		for (SubGraph s: list) {
+			int index = s.getV();
+			if (!map.containsKey(index)) {
+				map.put(index, new ArrayList<SubGraph>());
+			} 
+			map.get(index).add(s);
+		}
+
+		return map;
+	}
 
 	private static void printSSubGraphs(HyperGraph graph, Graphics g) {
 		List<SubGraph> list = graph.getSSubGraphs();
-		for (int i = 0; i< list.size(); i++) {
+		for (int i = 0; i < list.size(); i++) {
 			g.drawString(list.get(i).toString(), 10, i * 20 + 10);
 		}
-		
-		
+
 	}
 
 }
